d30621747a9317cafb36363a02f154112e0d2404
kolibriof <kolibri380@gmail.com>
2023-06-06 11:41:46 +0200

    initial error-page and login page setup

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import mockUser from './mockData.js/mockUser';
-import mockRepos from './mockData.js/mockRepos';
-import mockFollowers from './mockData.js/mockFollowers';
-import axios from 'axios';
+import React, { useState, useEffect, useContext } from "react";
+import mockUser from "./mockData.js/mockUser";
+import mockRepos from "./mockData.js/mockRepos";
+import mockFollowers from "./mockData.js/mockFollowers";
+import axios from "axios";
 
-const rootUrl = 'https://api.github.com';
+const rootUrl = "https://api.github.com";
+
+const globalContext = React.createContext();
+
+const GlobalContextProvider = ({ children }) => {
+  return (
+    <globalContext.Provider value={{ hello: "ddd" }}>
+      {children}
+    </globalContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(globalContext);
+};
+
+export { globalContext, GlobalContextProvider };
