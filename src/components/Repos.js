75875575ce05ec4162be2412a04dcac58dbfc51d
kolibriof <kolibri380@gmail.com>
2023-06-06 16:00:25 +0200

    charts functionality using reduce()

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,9 +1,92 @@
-import React from 'react';
-import styled from 'styled-components';
-import { GithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import React, { useMemo } from "react";
+import styled from "styled-components";
+import { useGlobalContext } from "../context/context";
+import { Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const { repos } = useGlobalContext();
+  let mostPopularLanguages = useMemo(() => {
+    const languages = repos.reduce((acc, value) => {
+      const { language } = value;
+      if (!language) {
+        return acc;
+      }
+      if (!acc[language]) {
+        acc[language] = { label: language, value: 1 };
+      } else {
+        acc[language] = { ...acc[language], value: acc[language].value + 1 };
+      }
+      return acc;
+    }, {});
+    return languages;
+  }, [repos]);
+  let mostPopularLanguageByStars = useMemo(() => {
+    const languages = repos.reduce((acc, value) => {
+      const { language, stargazers_count } = value;
+      if (!language || !stargazers_count) {
+        return acc;
+      }
+      if (!acc[language]) {
+        acc[language] = { label: language, value: stargazers_count };
+      } else {
+        acc[language] = {
+          ...acc[language],
+          value: acc[language].value + stargazers_count,
+        };
+      }
+      return acc;
+    }, {});
+    return languages;
+  }, [repos]);
+  let mostForkedRepo = useMemo(() => {
+    return repos.reduce((acc, value) => {
+      const { forks, name } = value;
+      if (!forks || forks === 0) {
+        return acc;
+      }
+      if (!acc[name]) {
+        acc[name] = { label: name, value: forks };
+      } else {
+        acc[name] = { ...acc[name], value: forks };
+      }
+      return acc;
+    }, {});
+  }, [repos]);
+  let mostWatchedRepos = useMemo(() => {
+    return repos.reduce((acc, value) => {
+      const { watchers, name } = value;
+      if (!watchers || watchers === 0) {
+        return acc;
+      }
+      if (!acc[name]) {
+        acc[name] = { label: name, value: watchers };
+      } else {
+        acc[name] = { ...acc[name], value: watchers };
+      }
+      return acc;
+    }, {});
+  }, [repos]);
+  mostWatchedRepos = Object.values(mostWatchedRepos)
+    .sort((a, b) => b.value - a.value)
+    .slice(0, 7);
+  mostForkedRepo = Object.values(mostForkedRepo)
+    .sort((a, b) => b.value - a.value)
+    .slice(0, 7);
+  mostPopularLanguages = Object.values(mostPopularLanguages).sort(
+    (a, b) => b.value - a.value
+  );
+  mostPopularLanguageByStars = Object.values(mostPopularLanguageByStars).sort(
+    (a, b) => b.value - a.value
+  );
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <Pie3D data={mostPopularLanguages} />
+        <Column3D data={mostWatchedRepos} />
+        <Doughnut2D data={mostPopularLanguageByStars} />
+        <Bar3D data={mostForkedRepo} />
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
